ce075cf7dbb43833de13bf3356d8b665beda9375
Vytenis Bivainis <monrealis@gmail.com>
2017-04-13 23:25:00 +0300

    Use subroutine to download file

--- download.sh
+++ download.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
-url=http://www.etsi.org/deliver/etsi_ts/102900_102999/102918/01.02.01_60/ts_102918v010201p.pdf
-file=asic.pdf
+function download {
+	test -f $1 || wget $2 -O $1
+}
 
-test -f $file || wget $url -O $file
-
-url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/MSO2010_DOCX/
-file=adoc-v2.docx
-
-test -f $file || wget $url -O $file
+download asic.pdf http://www.etsi.org/deliver/etsi_ts/102900_102999/102918/01.02.01_60/ts_102918v010201p.pdf
+download adoc-v2.docx https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/MSO2010_DOCX/
+download adoc-v2.pdf https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/ISO_PDF/
+download adoc-v2.odt https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/OO3_ODT/
 
 
-url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/ISO_PDF/
-file=adoc-v2.pdf
-
-test -f $file || wget $url -O $file
-
-url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/OO3_ODT/
-file=adoc-v2.odt
-
-test -f $file || wget $url -O $file
