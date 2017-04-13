d18488d218f0d73bc2f5b5bc233d6bfe608f3996
Vytenis Bivainis <monrealis@gmail.com>
2017-04-13 23:21:33 +0300

    Add adoc-v2 files

--- download.sh
+++ download.sh
@@ -4,3 +4,19 @@ url=http://www.etsi.org/deliver/etsi_ts/102900_102999/102918/01.02.01_60/ts_1029
 file=asic.pdf
 
 test -f $file || wget $url -O $file
+
+url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/MSO2010_DOCX/
+file=adoc-v2.docx
+
+test -f $file || wget $url -O $file
+
+
+url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/ISO_PDF/
+file=adoc-v2.pdf
+
+test -f $file || wget $url -O $file
+
+url=https://www.e-tar.lt/rs/legalact/8b67c9108f5e11e4a98a9f2247652cf4/format/OO3_ODT/
+file=adoc-v2.odt
+
+test -f $file || wget $url -O $file
